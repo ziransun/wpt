45606fe5e900c1d6af165bd792560511e96455ac
Ziran Sun <zsun@igalia.com>
2020-06-26 09:45:02 +0100

    Python3: non-api related changes

--- fetch/content-type/resources/content-type.py
+++ fetch/content-type/resources/content-type.py
@@ -1,15 +1,15 @@
 def main(request, response):
-    values = request.GET.get_list("value")
-    content = request.GET.first("content", "<b>hi</b>\n")
-    output =  "HTTP/1.1 200 OK\r\n"
-    output += "X-Content-Type-Options: nosniff\r\n"
-    if "single_header" in request.GET:
-        output += "Content-Type: " + ",".join(values) + "\r\n"
+    values = request.GET.get_list(b"value")  # ziran - 'latin-1' codec can't encode character '\u20ac' in position 33: ordinal not in range(256)
+    content = request.GET.first(b"content", b"<b>hi</b>\n")
+    output =  u"HTTP/1.1 200 OK\r\n"
+    output += u"X-Content-Type-Options: nosniff\r\n"
+    if b"single_header" in request.GET:
+        output += u"Content-Type: " + u",".join(values.decode("iso-8859-1")) + u"\r\n"  # convert list values
     else:
         for value in values:
-            output += "Content-Type: " + value + "\r\n"
-    output += "Content-Length: " + str(len(content)) + "\r\n"
-    output += "\r\n"
+            output += u"Content-Type: " + value.decode("iso-8859-1") + u"\r\n"
+    output += u"Content-Length: " + str(len(content)) + u"\r\n"
+    output += u"\r\n"
     output += content
     response.writer.write(output)
     response.close_connection = True
--- fetch/redirect-navigate/302-found-post-handler.py
+++ fetch/redirect-navigate/302-found-post-handler.py
@@ -1,13 +1,13 @@
 def main(request, response):
-    if request.method == "POST":
+    if request.method == u"POST":
         response.add_required_headers = False
         response.writer.write_status(302)
-        response.writer.write_header("Location", request.url)
+        response.writer.write_header(b"Location", request.url.encode("iso-8859-1"))
         response.writer.end_headers()
-        response.writer.write("")
-    elif request.method == "GET":
-        return ([("Content-Type", "text/plain")],
-                "OK")
+        response.writer.write(u"")
+    elif request.method == u"GET":
+        return ([(b"Content-Type", b"text/plain")],
+                u"OK")
     else:
-        return ([("Content-Type", "text/plain")],
-                "FAIL")
+        return ([(b"Content-Type", b"text/plain")],
+                u"FAIL")
